f7e16074a17cd1f06451ef2e879c4f0d0c061db5
linhhut <linhhn@vega.com.vn>
2017-10-12 09:11:42 +0700

    update

--- src/main/java/edu/java/self/configuration/SSUserDetailsService.java
+++ src/main/java/edu/java/self/configuration/SSUserDetailsService.java
@@ -10,30 +10,30 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class SSUserDetailsService implements UserDetailsService {
-	
+
 	@Override
 	public UserDetails loadUserByUsername(String username)
 			throws UsernameNotFoundException {
 		System.out.println("------ load user detail: "+ username);
-		
+
 		UserInfo userInfo = new UserInfo();
 		userInfo.setUsername("admin");
 		userInfo.setPassword("123456");
 		userInfo.setSessionId("asdfghjkl");
 		userInfo.setAuthorities(getAuthorities());
-		
+
 		return userInfo;
-//		return new org.springframework.security.core.userdetails.User("admin", "123456", getAuthorities());
+		//		return new org.springframework.security.core.userdetails.User("admin", "123456", getAuthorities());
 	}
 
 	private Set<GrantedAuthority> getAuthorities(){
-		
+
 		Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
 		GrantedAuthority grantedAuthority1 = new SimpleGrantedAuthority("ROLE_ADMIN"); // check whether ADMIN or ROLE_ADMIN
 		GrantedAuthority grantedAuthority2 = new SimpleGrantedAuthority("ROLE_USER");
 		authorities.add(grantedAuthority1);
 		authorities.add(grantedAuthority2);
-		
+
 		return authorities;
 	}
 }
